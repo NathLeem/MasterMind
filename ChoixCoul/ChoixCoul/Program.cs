769173706d82bd06b32751a36f3e409a6273b057
NathLeem <91312993+NathLeem@users.noreply.github.com>
2021-10-14 16:04:28 +0200

    upload de la première étape du mastemind

---
+++ ChoixCoul/ChoixCoul/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Mastermind
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] combi;
+            ChoixCoul(out combi);
+            Afficher(combi);
+        }
+        static void ChoixCoul(out int[] combi)
+        {
+            combi = new int[4];
+            for(int i = 0; 1 <= 3; i++)
+            {
+                Random rnd = new Random();
+                int valeur = rnd.Next(1, 7);
+                combi[i] = valeur;
+            }
+        }
+        static void Afficher(int[] combi)
+        {
+            for (int i = 0; 1 <= 3; i++)
+            {
+                Console.WriteLine(combi[i]);
+            }
+        }
+    }
+}
