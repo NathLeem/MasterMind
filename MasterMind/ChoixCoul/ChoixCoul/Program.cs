39c80c967429a143efb95dacca4e56d8b246477d
Leemans Nathan <200253@CCM-Asty-Moulin.local>
2021-11-26 09:10:48 +0100

    ajout pion rouge

--- MasterMind/ChoixCoul/ChoixCoul/Program.cs
+++ MasterMind/ChoixCoul/ChoixCoul/Program.cs
@@ -6,12 +6,18 @@ namespace Mastermind
     {
         static void Main(string[] args)
         {
-            Array propTabl;
+            int[] propTabl;
             int[] combi;
+            int nbrePionRouge;
+            string prop;
             ChoixCoul(out combi);
             Afficher(combi);
+            CoulJoueur(out propTabl);
+            PionRouge(out nbrePionRouge);
+
             Console.WriteLine();
-            Console.WriteLine("entrez votre combinaison:");
+            Console.WriteLine("entrez votre combinaison de 4 chiffres:");
+            prop = Console.ReadLine();
 
         }
 
@@ -26,11 +32,33 @@ namespace Mastermind
                 combi[i] = valeur;
             }
         }
+        // Choix de la combinaison par l'utilisateur
+        static void CoulJoueur(int prop,int[] propTabl)
+        {
+            for (int i = 0; i <= 3; i++)
+            {
+                propTabl[i] = prop;
+            }
+        }
+        // Affichage de la combinaison
         static void Afficher(int[] combi)
         {
             for (int i = 0; i <= 3; i++)
             {
                 Console.Write(combi[i]);
+            }
+        }
+        // L'ordinateur dit si il y a des pions rouge
+        static void PionRouge(int[] combi,int[] propTabl,out int nbrePionRouge)
+        {
+            nbrePionRouge = 0;
+            for (int i = 0; i <= 3; i++)
+            {
+                if (propTabl[i] == combi[i])
+                {
+                    nbrePionRouge++;
+                    Console.WriteLine($"Il y a {nbrePionRouge} pions rouge.");
+                }
             }
         }
     }
