165d7d2f3d9d250e5862871656f515a9789a7d61
NathLeem <91312993+NathLeem@users.noreply.github.com>
2021-12-02 16:10:40 +0100

    ajout pion blanc

--- MasterMind/ChoixCoul/ChoixCoul/Program.cs
+++ MasterMind/ChoixCoul/ChoixCoul/Program.cs
@@ -6,19 +6,9 @@ namespace Mastermind
     {
         static void Main(string[] args)
         {
-            int[] propTabl;
             int[] combi;
-            int nbrePionRouge;
-            string prop;
             ChoixCoul(out combi);
             Afficher(combi);
-            CoulJoueur(out propTabl);
-            PionRouge(out nbrePionRouge);
-
-            Console.WriteLine();
-            Console.WriteLine("entrez votre combinaison de 4 chiffres:");
-            prop = Console.ReadLine();
-
         }
 
         // la fonction permet à l'ordinateur de choisir une combinaison de quatre chiffres
@@ -26,39 +16,17 @@ namespace Mastermind
         {
             combi = new int[4];
             Random rnd = new Random();
-            for(int i = 0; i <= 3; i++)
+            for(int i = 0; 1 <= 3; i++)
             {
                 int valeur = rnd.Next(1, 7);
                 combi[i] = valeur;
             }
         }
-        // Choix de la combinaison par l'utilisateur
-        static void CoulJoueur(int prop,int[] propTabl)
-        {
-            for (int i = 0; i <= 3; i++)
-            {
-                propTabl[i] = prop;
-            }
-        }
-        // Affichage de la combinaison
         static void Afficher(int[] combi)
         {
-            for (int i = 0; i <= 3; i++)
+            for (int i = 0; 1 <= 3; i++)
             {
-                Console.Write(combi[i]);
-            }
-        }
-        // L'ordinateur dit si il y a des pions rouge
-        static void PionRouge(int[] combi,int[] propTabl,out int nbrePionRouge)
-        {
-            nbrePionRouge = 0;
-            for (int i = 0; i <= 3; i++)
-            {
-                if (propTabl[i] == combi[i])
-                {
-                    nbrePionRouge++;
-                    Console.WriteLine($"Il y a {nbrePionRouge} pions rouge.");
-                }
+                Console.WriteLine(combi[i]);
             }
         }
     }
---
+++ MasterMind/MasterMind/ChoixCoul/ChoixCoul/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace Mastermind
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] propTabl;
+            int[] combi;
+            int nbrePionRouge = 0;
+            int nbrePionBlanc = 0;
+            int nbreChance = 12;
+            string prop;
+
+            ChoixCoul(out combi);
+            Afficher(combi);
+            while (nbrePionRouge != 4 || nbreChance != 0)
+            {
+                Console.WriteLine();
+                Console.WriteLine("entrez votre combinaison de 4 chiffres:");
+                prop = Console.ReadLine();
+                CoulJoueur(prop,out propTabl);
+                PionRouge(combi, propTabl, out nbrePionRouge);
+                PionBlanc(combi, propTabl, out nbrePionBlanc);
+                nbreChance = nbreChance - 1;
+            }
+        }
+        // la fonction permet à l'ordinateur de choisir une combinaison de quatre chiffres
+        static void ChoixCoul(out int[] combi)
+        {
+            combi = new int[4];
+            Random rnd = new Random();
+            for(int i = 0; i <= 3; i++)
+            {
+                int valeur = rnd.Next(1, 7);
+                combi[i] = valeur;
+            }
+        }
+        // Choix de la combinaison par l'utilisateur
+        static void CoulJoueur(string prop,out int[] propTabl)
+        {
+            propTabl = new int[4];
+            for (int i = 0; i <= 3; i++)
+            {
+                propTabl[i] = int.Parse(prop[i].ToString());
+            }
+        }
+        // Affichage de la combinaison
+        static void Afficher(int[] combi)
+        {
+            for (int i = 0; i <= 3; i++)
+            {
+                Console.Write(combi[i]);
+            }
+        }
+        // L'ordinateur dit si il y a des pions rouge
+        static void PionRouge(int[] combi,int[] propTabl,out int nbrePionRouge)
+        {
+            nbrePionRouge = 0;
+            for (int i = 0; i <= 3; i++)
+            {
+                if (propTabl[i] == combi[i])
+                {
+                    nbrePionRouge++;
+                    propTabl[i] = -1;
+                    combi[i] = -2;
+                    
+                }    
+            }
+            Console.WriteLine($"Il y a {nbrePionRouge} pions rouge.");
+        }
+        // l'ordinateur dit s'il y a des pions blanc.
+        static void PionBlanc(int[] combi,int[] propTabl,out int nbrePionBlanc)
+        {
+            nbrePionBlanc = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (propTabl[i] == combi[j])
+                    {
+                       nbrePionBlanc++;
+                       propTabl[i] = -1; 
+                    }
+                    
+                }
+            }
+            Console.WriteLine($"Il y a {nbrePionBlanc} pions blanc.");
+        }
+    }
+}
